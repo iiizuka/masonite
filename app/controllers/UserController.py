4072bf900b6c4fd90f0fff8db71e83b3b9852176
iizuka <satoru.iizuka@trick-studio.jp>
2022-06-02 22:18:43 +0900

    add controller

--- app/controllers/UserController.py
+++ app/controllers/UserController.py
@@ -1,5 +1,7 @@
 from masonite.controllers import Controller
 from masonite.views import View
+from masonite.request import Request
+from masonite.User import User
 
 
 class UserController(Controller):
@@ -12,7 +14,9 @@ class UserController(Controller):
     def store(self, view: View):
         return view.render("")
 
-    def show(self, view: View):
+    def show(self, view: View, request: Request):
+        User.find(request.param('user_id'))
+
         return view.render("")
 
     def edit(self, view: View):
