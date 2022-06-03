38480240e610dd3eab0ddcb4475181d67a8b0b68
iizuka <satoru.iizuka@trick-studio.jp>
2022-06-03 22:10:24 +0900

    update routing

--- routes/web.py
+++ routes/web.py
@@ -1,6 +1,6 @@
 from masonite.routes import Route
 
 ROUTES = [
-    Route.get("/@id", "WelcomeController@show"),
+    Route.get("/", "WelcomeController@show"),
     Route.resource("user", "UserController"),
 ]
