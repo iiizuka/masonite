907b6c43e16875f03cb97eccb4fad7cccc6dae32
iizuka <satoru.iizuka@trick-studio.jp>
2022-06-04 22:32:12 +0900

    update seeder

--- databases/seeds/user_table_seeder.py
+++ databases/seeds/user_table_seeder.py
@@ -8,11 +8,20 @@ from app.models.User import User
 class UserTableSeeder(Seeder):
     def run(self):
         """Run the database seeds."""
+        User.truncate()
+
         User.create(
             {
                 "name": "Joe",
-                "email": "user@example.com",
-                "password": Hash.make("secret"),
-                "phone": "+123456789",
+                "email": "user1@example.com",
+                "password": Hash.make("password"),
+                "phone": "09011111111",
+            }
+        ).create(
+            {
+                "name": "Anna",
+                "email": "user2@example.com",
+                "password": Hash.make("password"),
+                "phone": "09022222222",
             }
         )
