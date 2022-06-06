608e7f5bd36df4229522d8b2721a2c20f3500a14
iizuka <satoru.iizuka@trick-studio.jp>
2022-06-06 22:06:04 +0900

    update add files

--- app/controllers/UserController.py
+++ app/controllers/UserController.py
@@ -6,24 +6,54 @@ from app.models.User import User
 
 class UserController(Controller):
     def index(self, view: View):
-        return view.render("")
+        """ユーザ一覧."""
+        return view.render("user.index", {
+            "users" : User.all()
+        })
 
     def create(self, view: View):
-        return view.render("")
+        """ユーザ登録画面."""
+        return view.render("user.create")
 
-    def store(self, view: View):
-        return view.render("")
+    def store(self, view: View, request: Request):
+        """ユーザ登録."""
+        User.create(
+            name=request.input('name'),
+            email=request.input('email'),
+        )
+
+        return view.render("user.index", {
+            "users" : User.all()
+        })
 
     def show(self, view: View, request: Request):
-        return view.render("welcome", {
+        """ユーザ詳細."""
+        return view.render("user.show", {
             "user": User.find(request.param("id"))
         })
 
-    def edit(self, view: View):
-        return view.render("")
+    def edit(self, view: View, request: Request):
+        """ユーザ編集画面."""
+        return view.render("user.edit", {
+            "user": User.find_or_404(request.param("id"))
+        })
 
-    def update(self, view: View):
-        return view.render("")
+    def update(self, view: View, request: Request):
+        """ユーザ編集."""
+        user = User.find_or_404(request.param("id"))
 
-    def destroy(self, view: View):
-        return view.render("")
+        user.fill(request).save()
+
+        return view.render("user.index", {
+            "users" : User.all()
+        })
+
+    def destroy(self, view: View, request: Request):
+        """ユーザ削除."""
+        user = User.find_or_404(request.param("id"))
+
+        user.delete()
+
+        return view.render("user.index", {
+            "users" : User.all()
+        })
